c815078598945d5f6d311d87003abe091656e1a5
Pedro Polanco <pedropolanco02@gmail.com>
2021-02-22 11:05:55 -0500

    get mapping added to controller and service impl added

--- server/src/main/java/com/example/server/controller/PokemonsController.java
+++ server/src/main/java/com/example/server/controller/PokemonsController.java
@@ -1,6 +1,8 @@
 package com.example.server.controller;
 
 import com.example.server.model.Pokemons;
+import com.example.server.service.PokemonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pokemon")
 
 public class PokemonsController {
-//@GetMapping
-//Iterable<Pokemons> getPokemons(){return}
+    @Autowired
+    PokemonService pokemonService;
+    @GetMapping
+    Iterable<Pokemons> getPokemons(){return pokemonService.getPokemons();}
 }
--- server/src/main/java/com/example/server/service/PokemonServiceImpl.java
+++ server/src/main/java/com/example/server/service/PokemonServiceImpl.java
@@ -1,4 +1,31 @@
 package com.example.server.service;
 
-public class PokemonServiceImpl {
+import com.example.server.model.Pokemons;
+import com.example.server.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+public class PokemonServiceImpl implements PokemonService{
+    @Autowired
+    PokemonRepository pokemonRepository;
+    @Override
+    public Iterable<Pokemons> getPokemons() {
+        return pokemonRepository.findAll();
+    }
+
+    @Override
+    public Pokemons createPokemons(Pokemons pokemons) {
+        return pokemonRepository.save(pokemons);
+    }
+
+    @Override
+    public Pokemons updatePokemons(Pokemons pokemons) {
+        return pokemonRepository.save(pokemons);
+    }
+
+    @Override
+    public HttpStatus deletePokemons(Long id) {
+         pokemonRepository.deleteById(id);
+         return HttpStatus.OK;
+    }
 }
